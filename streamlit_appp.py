d735c6d8f26a21f3509a7b04859dc24aa949448e
cristianProyects <119350105+cristianProyects@users.noreply.github.com>
2023-03-02 17:35:06 -0600

    Update streamlit_appp.py

--- streamlit_appp.py
+++ streamlit_appp.py
@@ -57,7 +57,7 @@ if streamlit.button('Get fruit load list'):
    my_data_rows = get_fruit_load_list()
    streamlit.dataframe(my_data_rows)
    
-   streamlit.stop();
+streamlit.stop();
 
 streamlit.header("what fruit would you like to add?")
 
